2f7365aece0b72e9241dba39de15359e36b5f201
alejandroFMA <alexfmarquez@gmail.com>
2023-12-09 18:13:54 +0100

    fix styles visibility

--- src/components/Main/Home/HomeList/HomeCard/HomeCard.jsx
+++ src/components/Main/Home/HomeList/HomeCard/HomeCard.jsx
@@ -1,12 +1,19 @@
 import React from "react";
 import {Link} from 'react-router-dom'
+import { scroll } from "react-scroll";
 const HomeCard = ({id, title, image}) => {
+
+  const scrollToTop = () => {
+    scroll.scrollToTop();
+  };
+  
     return (
       <>
           <article className="cardPokemon">
           <h3>#{id}</h3>
           <Link
           to={`/pokemon/${id}`}
+          onClick={scrollToTop}
           style={{ textDecoration: "none", color: "inherit" }}>
           <img src={image} alt={`Pokemon ${title}`} /></Link>
           <p className="pokeName">{title}</p>
